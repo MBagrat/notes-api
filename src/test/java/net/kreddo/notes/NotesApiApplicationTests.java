75afcf224b51cad9db35a776c3ce52cf0e83db6e
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2021-01-16 20:29:52 +0400

    initial commit

---
+++ src/test/java/net/kreddo/notes/NotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kreddo.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
